6a9f5643908c01b56c3ec4f121bccd25348162d1
Dmitry Abramenko <dmitry.abramenko1995@gmail.com>
2016-11-24 02:39:23 +0600

    adding normileze.css, npm packages etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
 const image = require('gulp-image');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 var path = {
@@ -17,7 +19,20 @@ gulp.task('default', ['build', 'serve', 'watch']);
 
 gulp.task('css', function () {
   return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
     .pipe(concat('style.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+gulp.task('css-min', function () {
+  return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
+    .pipe(concat('style.css'))
+    .pipe(cssmin())
     .pipe(gulp.dest(path.dist.css));
 });
 
@@ -32,11 +47,12 @@ gulp.task('image', function () {
     .pipe(image())
     .pipe(gulp.dest('./dist/image/'));
 });
-gulp.task('font-css', function () {
+gulp.task('font', function () {
    gulp.src('src/font-awesome-4.7.0/**/*')
     .pipe(gulp.dest('./dist/font-awesome/'))
 });
-gulp.task('build', ['html', 'css', 'image', 'font-css']);
+gulp.task('build', ['html', 'css', 'image', 'font']);
+gulp.task('prod', ['html', 'css-min', 'image', 'font']);
 
 
 gulp.task('watch', function () {
